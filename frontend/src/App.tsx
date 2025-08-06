2990bfa70e0820d56c9a7049de57001f39d6dfd5
HUPPPPPP <131924704+Krittapas2004@users.noreply.github.com>
2025-08-07 07:34:14 +1200

    feat(Other-Courses): Added a title to test if the router is working correctly for other courses page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProgramNav from './pages/ProgramNav';
 import BachelorIT from './pages/BachelorIT';
+import BachelorBusiness from './pages/BachelorBusiness';
+import BachelorCulinary from './pages/BachelorCulinary';
+import BachelorDesign from './pages/BachelorDesign';
 import './style.css';
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <ProgramNav />
       <Routes>
         <Route path="/" element={<BachelorIT />} />
+        <Route path="/design" element={<BachelorDesign />} />
+        <Route path="/business" element={<BachelorBusiness />} />
+        <Route path="/culinary" element={<BachelorCulinary />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/BachelorBusiness.tsx
@@ -0,0 +1,11 @@
+const BachelorBusiness = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold text-indigo-700">
+        Bachelor of Business
+      </h1>
+    </div>
+  );
+};
+
+export default BachelorBusiness;
