be5a7c102b1f800822b770bf840277cad953dc56
HUPPPPPP <131924704+Krittapas2004@users.noreply.github.com>
2025-08-10 21:00:12 +1200

    feat(searchBar): implement search bar for users to search courses

--- frontend/src/pages/BachelorCulinary.tsx
+++ frontend/src/pages/BachelorCulinary.tsx
@@ -1,9 +1,15 @@
+import SearchBar from "../components/SearchBar";
+
 const BachelorCulinary = () => {
   return (
     <div className="text-center mt-10">
       <h1 className="text-3xl font-bold text-indigo-700">
         Bachelor of Culinary
       </h1>
+
+      <div className="max-w-2xl mx-auto">
+        <SearchBar />
+      </div>
     </div>
   );
 };
